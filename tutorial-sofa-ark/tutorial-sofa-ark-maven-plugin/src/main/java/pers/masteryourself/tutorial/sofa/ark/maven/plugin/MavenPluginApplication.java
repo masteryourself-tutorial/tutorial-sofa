ea34b4bd8cb952c2cf0765f9d0ed12706189d893
ruanrenzhao <renzhao.ruan@foxmail.com>
2020-03-14 17:11:14 +0800

    tutorial-sofa-ark-maven-plugin 工程提交

---
+++ tutorial-sofa-ark/tutorial-sofa-ark-maven-plugin/src/main/java/pers/masteryourself/tutorial/sofa/ark/maven/plugin/MavenPluginApplication.java
@@ -0,0 +1,20 @@
+package pers.masteryourself.tutorial.sofa.ark.maven.plugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>description : MavenPluginApplication
+ *
+ * @author : masteryourself
+ * @version : 1.0.0
+ * @date : 2020/3/14 16:37
+ */
+@SpringBootApplication
+public class MavenPluginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenPluginApplication.class, args);
+    }
+
+}
---
+++ tutorial-sofa-ark/tutorial-sofa-ark-maven-plugin/src/main/java/pers/masteryourself/tutorial/sofa/ark/maven/plugin/controller/TestController.java
@@ -0,0 +1,21 @@
+package pers.masteryourself.tutorial.sofa.ark.maven.plugin.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>description : TestController
+ *
+ * @author : masteryourself
+ * @version : 1.0.0
+ * @date : 2020/3/14 16:43
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "just for test!!!";
+    }
+
+}
